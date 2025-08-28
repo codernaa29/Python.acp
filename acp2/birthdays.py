4e7cdb4f5869bd77ca1297bd54014246e1de78ae
codernaa29 <01717360938hawa@gmail.com>
2025-08-29 01:50:15 +0600

    2nd commit

---
+++ acp2/birthdays.py
@@ -0,0 +1,26 @@
+# Some birthday variables
+name1 = "Arthi"
+birthday1 = "8th June, 2009"
+print(name1, "has birthday on", birthday1)
+
+name2 = "Noshin"
+birthday2 = "11th April, 2009"
+print(name2, "has birthday on", birthday2)
+
+name3 = "Raidah"
+birthday3 = "14th May, 2009"
+print(name3, "has birthday on", birthday3)
+
+name4 = "Sabreen"
+birthday4 = "26th June, 2009"
+print(name4, "has birthday on", birthday4)
+
+name5 = "Afseen"
+birthday5 = "19th July, 2009"
+print(name5, "has birthday on", birthday5)
+
+# user input
+your_name = input("What is your name: ")
+your_birthday = input("When is your birthday : ")
+print(your_name, "has birthday on", your_birthday)
+

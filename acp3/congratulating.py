5e6e5747467eaafdd87ff1c8231808e12cc37059
codernaa29 <01717360938hawa@gmail.com>
2025-09-26 01:04:41 +0600

    3rd commit

---
+++ acp3/congratulating.py
@@ -0,0 +1,9 @@
+# the story
+print("Ram is congratulating his friend for getting a gold medal in the state games.")
+
+# input the word in lower case
+word = "congratulations"
+
+# convert the word into upper case
+print(word.upper())
+

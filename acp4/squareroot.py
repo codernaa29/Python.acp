88a651bb619c55c82b7b4bb8b725e7ff98be91d9
codernaa29 <01717360938hawa@gmail.com>
2025-10-17 00:13:08 +0600

    4th commit

---
+++ acp4/squareroot.py
@@ -0,0 +1,9 @@
+# Taking number input from user
+number = int(input("Enter a number to find its square root: "))
+
+# Calculating square root 
+squre_root = number ** 0.5
+
+# Displaying the result
+print("The square root of", number, "is:", squre_root)
+

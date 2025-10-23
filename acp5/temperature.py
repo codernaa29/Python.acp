4a4a07001a68f1c3351fbe492268dbc231112fc7
codernaa29 <01717360938hawa@gmail.com>
2025-10-24 01:44:00 +0600

    5th commit

---
+++ acp5/temperature.py
@@ -0,0 +1,8 @@
+# checking what temperature is suitable for wearing light clothes
+
+temperature = float(input("Enter today's temperature: "))
+
+if temperature >= 25:
+    print("It's warm outside. You can wear light clothes.")
+else:
+    print("It's too cold for light clothes. Better wear something warm.")

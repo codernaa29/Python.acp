2801ed4caa6ab51dba4041c99053c1a203bc8d55
codernaa29 <01717360938hawa@gmail.com>
2025-12-18 21:12:32 +0600

    8th commit

---
+++ acp8/checkage.py
@@ -0,0 +1,11 @@
+# Take input of age from the user
+age = int(input("Enter your age: "))
+
+# Check whether the age is between 10 and 20
+if age >= 10:
+    if age <= 20:
+        print("Age is between 10 and 20 years")
+    else:
+        print("Age is not between 10 and 20 years")
+else:
+    print("Age is not between 10 and 20 years")
